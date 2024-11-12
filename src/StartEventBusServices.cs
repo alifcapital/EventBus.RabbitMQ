fa0474cd92588698d407ee06ba976c9d945af6d1
akbar <a.shoimov@alif.tj>
2024-11-12 18:35:15 +0500

    Move to primary constructor

--- src/StartEventBusServices.cs
+++ src/StartEventBusServices.cs
@@ -5,29 +5,22 @@ using Microsoft.Extensions.Logging;
 
 namespace EventBus.RabbitMQ;
 
-internal class StartEventBusServices : BackgroundService
+internal class StartEventBusServices(
+    IEventSubscriberManager subscriberManager,
+    IEventPublisherManager publisherManager,
+    ILogger<StartEventBusServices> logger)
+    : BackgroundService
 {
-    private readonly IEventSubscriberManager _subscriberManager;
-    private readonly IEventPublisherManager _publisherManager;
-    private readonly ILogger<StartEventBusServices> _logger;
-
-    public StartEventBusServices(IEventSubscriberManager subscriberManager, IEventPublisherManager publisherManager, ILogger<StartEventBusServices> logger)
-    {
-        _publisherManager = publisherManager;
-        _subscriberManager = subscriberManager;
-        _logger = logger;
-    }
-
     protected override Task ExecuteAsync(CancellationToken stoppingToken)
     {
         try
         {
-            _publisherManager.CreateExchangeForPublishers();
-            _subscriberManager.CreateConsumerForEachQueueAndStartReceivingEvents();
+            publisherManager.CreateExchangeForPublishers();
+            subscriberManager.CreateConsumerForEachQueueAndStartReceivingEvents();
         }
         catch (Exception e)
         {
-            _logger.LogError(e, "Error while configuring publisher and subscriber of the RabbitMQ.");
+            logger.LogError(e, "Error while configuring publisher and subscriber of the RabbitMQ.");
         }
         
         return Task.CompletedTask;
