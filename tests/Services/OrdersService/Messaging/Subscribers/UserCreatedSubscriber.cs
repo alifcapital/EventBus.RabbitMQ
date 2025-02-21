eccfa9bd2489bb1637c954446695ccdd438e5517
Mirolim Majidov <mirolimmajidov@outlook.com>
2025-02-21 15:26:20 +0500

    Improved the examples

--- tests/Services/OrdersService/Messaging/Subscribers/UserCreatedSubscriber.cs
+++ tests/Services/OrdersService/Messaging/Subscribers/UserCreatedSubscriber.cs
@@ -1,32 +1,36 @@
 using EventBus.RabbitMQ.Subscribers.Models;
-using OrdersService.Messaging.Events;
-using OrdersService.Messaging.Events.Module1;
 
 namespace OrdersService.Messaging.Subscribers;
 
-public class UserCreatedSubscriber(ILogger<UserCreatedSubscriber> logger) : IEventSubscriber<UserCreated>
+public class UserCreatedSubscriber1(ILogger<UserCreatedSubscriber1> logger) : IEventSubscriber<Events.Module1.UserCreated>
 {
-    public async Task HandleAsync(UserCreated @event)
+    public async Task HandleAsync(Events.Module1.UserCreated @event)
     {
         logger.LogInformation("Id ({Id}): '{UserName}' user is created with the {UserId} id", @event.EventId,
             @event.UserName, @event.UserId);
-
+        
         await Task.CompletedTask;
     }
 }
 
-public class UserCreatedSubscriber2 : IEventSubscriber<UserCreated>
+public class UserCreatedSubscriber2(ILogger<UserCreatedSubscriber2> logger) : IEventSubscriber<Events.Module1.UserCreated>
 {
-    public async Task HandleAsync(UserCreated @event)
+    public async Task HandleAsync(Events.Module1.UserCreated @event)
     {
+        logger.LogInformation("Id ({Id}): '{UserName}' user is created with the {UserId} id", @event.EventId,
+            @event.UserName, @event.UserId);
+        
         await Task.CompletedTask;
     }
 }
 
-public class UserCreatedSubscriber3 : IEventSubscriber<Events.Module2.UserCreated>
+public class UserCreatedSubscriber3(ILogger<UserCreatedSubscriber3> logger) : IEventSubscriber<Events.Module2.UserCreated>
 {
     public async Task HandleAsync(Events.Module2.UserCreated @event)
     {
+        logger.LogInformation("Id ({Id}): '{UserName}' user is created with the {UserId} id", @event.EventId,
+            @event.UserName, @event.UserId);
+        
         await Task.CompletedTask;
     }
 }
