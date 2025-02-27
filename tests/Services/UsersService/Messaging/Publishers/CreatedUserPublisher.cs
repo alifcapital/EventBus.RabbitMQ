e05941dabd9e19486286d2e4edf66e70c6fdd33c
Mirolim Majidov <mirolimmajidov@outlook.com>
2025-02-27 12:24:47 +0500

    Uncommented

--- tests/Services/UsersService/Messaging/Publishers/CreatedUserPublisher.cs
+++ tests/Services/UsersService/Messaging/Publishers/CreatedUserPublisher.cs
@@ -4,19 +4,12 @@ using UsersService.Messaging.Events.Publishers;
 
 namespace UsersService.Messaging.Publishers;
 
-// public class CreatedUserPublisher : IMessageBrokerEventPublisher<UserCreated>
-// {
-//     private readonly IEventPublisherManager _eventPublisher;
-//     
-//     public CreatedUserPublisher(IEventPublisherManager eventPublisher)
-//     {
-//         _eventPublisher = eventPublisher;
-//     }
-//
-//     public async Task PublishAsync(UserCreated @event, string eventPath)
-//     {
-//         _eventPublisher.Publish(@event);
-//         //Add you logic
-//         await Task.CompletedTask;
-//     }
-// }
+public class CreatedUserPublisher(IEventPublisherManager eventPublisher) : IMessageBrokerEventPublisher<UserCreated>
+{
+    public async Task PublishAsync(UserCreated @event, string eventPath)
+    {
+        eventPublisher.Publish(@event);
+        //Add you logic
+        await Task.CompletedTask;
+    }
+}
