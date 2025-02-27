099b5256450fb9b23dc719a639fab18d66645d47
Mirolim Majidov <mirolimmajidov@outlook.com>
2025-02-27 13:38:35 +0500

    Hotfix

--- src/Publishers/Messaging/MessageBrokerEventPublisher.cs
+++ src/Publishers/Messaging/MessageBrokerEventPublisher.cs
@@ -1,3 +1,4 @@
+using EventBus.RabbitMQ.Exceptions;
 using EventBus.RabbitMQ.Publishers.Managers;
 using EventBus.RabbitMQ.Publishers.Models;
 using EventStorage.Outbox.Models;
@@ -9,10 +10,13 @@ namespace EventBus.RabbitMQ.Publishers.Messaging;
 /// The implementation of the message broker event publisher to publish an event to the RabbitMQ message broker when the application is configured to use the outbox pattern.
 /// </summary>
 /// <param name="eventPublisher">The event publisher manager to publish the event to the RabbitMQ message broker.</param>
-internal class MessageBrokerEventPublisher(IEventPublisherManager eventPublisher) : IMessageBrokerEventPublisher
+internal class MessageBrokerEventPublisher(IEventPublisherManager eventPublisher = null) : IMessageBrokerEventPublisher
 {
     public async Task PublishAsync(IOutboxEvent @event, string eventPath)
     {
+        if(eventPublisher == null)
+            throw new EventBusException("There is an outbox event to publish that through the message broker, but the RabbitMQ is not enabled.");
+        
         eventPublisher.Publish((IPublishEvent)@event);
         await Task.CompletedTask;
     }
